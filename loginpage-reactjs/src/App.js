f24aa416d72f068470d1224a4303ca447cd91bd7
Sohaib-Mehmood <sohaibmehmood776@gmail.com>
2023-04-03 13:41:34 +0500

    starter-files

--- loginpage-reactjs/src/App.js
+++ loginpage-reactjs/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+var isLoggedIn = true;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      if(isLoggedIn === true){
+        <h1>Hello</h1>
+      }else{
+        <form className = "form">
+        <input type='text' placeholder='Username'/>
+        <input type='password' placeholder='Password' />
+        <button type='submit'>Login</button>
+      </form>
+      }
     </div>
   );
 }
